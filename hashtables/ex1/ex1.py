1b17b4fc6fd0abc349942c780f0f5f07564f2122
Eian Carter <eian@bricleir.com>
2020-06-06 15:47:46 -0700

    ex2 passed all tests

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,16 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
 
+    rack = dict()
+
+    for weight in weights:
+        Answer = limit - weight
+        if Answer not in rack:
+            rack[Answer] = 1
+        else:
+            return (weights.index(Answer), weights.index(weight))
     return None
+
+
+print(get_indices_of_item_weights([4,4], 2, 8))
